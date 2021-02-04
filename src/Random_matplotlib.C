9a87d74550d38a2717175be00ec3427bdbbf42a4
Christopher Rogan <christopherrogan@ChristophersMBP.attlocal.net>
2021-02-04 17:34:34 -0600

    added comments to all the code

--- src/Random_matplotlib.C
+++ src/Random_matplotlib.C
@@ -7,8 +7,14 @@ using namespace std;
 
 namespace plt = matplotlibcpp;
 
+////////////////
+// Random class
+////////////////
+// class to generate pseudo-random numbers
 class Random {
+  
 public:
+  // constructor for class instance (has optional seed argument, with default value
   Random(unsigned long long seed = 5555){
     m_v = 4101842887655102017LL;
     m_w = 1;
@@ -21,10 +27,12 @@ public:
     int64();
   }
 
+  // destructor for our class (no internal members that need to be deleted)
   virtual ~Random(){
 
   }
 
+  // function returns a random 64 bit integer
   unsigned long long int64(){
     m_u = m_u * 2862933555777941757LL + 7046029254386353087LL;
     m_v ^= m_v >> 17;
@@ -37,15 +45,19 @@ public:
     return (x + m_v) ^ m_w;
   }
 
+  // function returns a random 32 bit integer
   unsigned int int32(){
     return (unsigned int)int64(); 
   }
 
+  // function returns a random double between (0, 1) (uniform)
   double rand(){
     return 5.42101086242752217E-20 * int64();
   }
 
 private:
+  // private member variables stored inside class instance (random number
+  // generator has an internal memory)
   unsigned long long m_seed;
   unsigned long long m_u;
   unsigned long long m_v;
@@ -53,10 +65,12 @@ private:
 
 };
 
+// main function for compiled executable
 int main(int argc, char* argv[]){
   bool printhelp = false;
   long seed = 5555;
-  
+
+  // parsing any command line options added by the user
   for(int i = 0; i < argc; i++){
     if(strncmp(argv[i],"--help", 6) == 0){
       printhelp = true;
@@ -69,6 +83,8 @@ int main(int argc, char* argv[]){
       seed = std::stol(argv[i]);
     }
   }
+
+  // print the executable usage options if the user adds -h or --help
   if(printhelp){
     cout << "Usage: " << argv[0] << " [options]" << endl;
     cout << "  options:" << endl;
@@ -78,24 +94,32 @@ int main(int argc, char* argv[]){
     return 0;
   }
 
+  // declare an instance of our Random class
   Random  random(seed);
+  // declare an instance of our Random class, with a pointer to the instance
   Random* random_ptr = new Random(seed);
   
   int N = 5000; // 5000 random data points
+
+  // fill a vector with pseudo-random numbers
   vector<double> x;
   for(int i = 0; i < N; ++i){
     x.push_back(random.rand());
-    //x.push_back(random_ptr->rand());
+    //x.push_back(random_ptr->rand()); // this is the syntax for using the pointer
   }
 
   plt::figure(); // declare a new figure (optional if only one is used)
-  
+
+  // formating options for our figure
   plt::hist(x, 50, "b", 0.75);
-  plt::title("Uniform random number"); // set a title
+  plt::title("Uniform random number"); 
   plt::xlabel("x");
   plt::ylabel("Counts");
 
   plt::show();
-  plt::savefig("random.pdf"); // save the figure 
+  plt::savefig("random.pdf"); // save the figure
+
+  // clean up memory we allocated in program
+  delete random_ptr;
 }
 
